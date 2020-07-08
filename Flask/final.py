4c1e5b42fb2878c27dbe3511cf4b61ab78e9538a
Fatih Inal <ffatihinal@gmail.com>
2020-07-09 00:47:39 +0300

    First step of combining with MySQL

--- Flask/final.py
+++ Flask/final.py
@@ -3,7 +3,27 @@ from flask_mysqldb import MySQL
 from wtforms import Form,StringField,TextAreaField,PasswordField,validators
 from passlib.hash import sha256_crypt
 
+
 app = Flask(__name__)
+app.config["MYSQL_HOST"] = "localhost" # If this programme is going to run on another host(cloud or rental host), 
+                                       #its adress has to written here
+app.config["MYSQL_USER"]        = "root" # Default value
+app.config["MYSQL_PASWORD"]     = ""     # Default value
+app.config["MYSQL_DB"]          = "final_database"
+app.config["MYSQL_CURSORCLASS"] = "DictCursor"
+mysql = MySQL(app) # Sets relationship btw MySQL & Flask
+
+# Creating User Registration Form
+class RegisterForm(Form):
+    name = StringField("Name & Surname",validators = [validators.Length(min = 2,max = 30)])
+    username = StringField("Username",validators = [validators.Length(min = 4,max = 15)])
+    email = StringField("E-mail Adress",validators = [validators.Email(message = "Please enter a valid E-mail adress")])
+    password = PasswordField("Password",validators = [
+        validators.DataRequired("Please designate a password"),
+        validators.EqualTo(fieldname = "confirm",message = "Passoword does not match")])
+    confirm = PasswordField("Confirm Password")
+
+
 @app.route("/")
 def fatih():
     list = [1,2,10,"Fatih","Python"]
@@ -24,6 +44,11 @@ def about():
 def material(number):
     return "Number of material  " + number
 
+# Register (Log In)
+@app.route("/register")
+def register():
+    return render_template("final_register.html")
+    
 
 
 if __name__ == "__main__":
